06bea5ce7ac8da959c11da9dd767fb34d93d9282
Alvaro Leal <pursuance@gmail.com>
2020-10-19 19:15:49 +0200

    Use react vis

--- components/radar/radar-component.js
+++ components/radar/radar-component.js
@@ -1,141 +1,52 @@
 import React from 'react';
-import { Group } from '@visx/group';
-import { scaleLinear } from '@visx/scale';
-import { Point } from '@visx/point';
-import { Line, LineRadial } from '@visx/shape';
+import { format } from 'd3-format';
+import { RadarChart } from 'react-vis';
 
-// const expertiseClassification = {
-//   'No knowledge': 0,
-//   'Basic knowledge': 1,
-//   Competent: 2,
-//   Expert: 3
-// };
+const wideFormat = format('.3r');
 
-const orange = '#ff9933';
-export const pumpkin = '#f5810c';
-const silver = '#d9d9d9';
-export const background = '#FFF';
+export default function BasicRadarChart(props) {
+  const { width, height, data } = props;
+  // We need the domainNames of all categories, not only basic data
+  const basicData = data.find(d => d.name === 'basic knowledge');
+  const domainNames = Object.keys(basicData).filter(key => key !== 'name');
+  const domains = domainNames.map(name => ({
+    name,
+    domain: [0, 18],
+    getValue: (d) => d[name]
+  }))
 
-const degrees = 360;
-
-const genAngles = (length) =>
-  [...new Array(length + 1)].map((_, i) => ({
-    angle: i * (degrees / length)
-  }));
-
-const genPoints = (length, radius) => {
-  const step = (Math.PI * 2) / length;
-  return [...new Array(length)].map((_, i) => ({
-    x: radius * Math.sin(i * step),
-    y: radius * Math.cos(i * step)
-  }));
-};
-
-function genPolygonPoints(
-  dataArray,
-  scale,
-  getValue
-) {
-  const step = (Math.PI * 2) / dataArray.length;
-  const points = new Array(dataArray.length).fill({
-    x: 0,
-    y: 0
-  });
-  const pointString = new Array(dataArray.length + 1)
-    .fill('')
-    .reduce((res, _, i) => {
-      if (i > dataArray.length) return res;
-      const xVal = scale(getValue(dataArray[i - 1])) * Math.sin(i * step);
-      const yVal = scale(getValue(dataArray[i - 1])) * Math.cos(i * step);
-      points[i - 1] = { x: xVal, y: yVal };
-      res += `${xVal},${yVal} `;
-      return res;
-    });
-
-  return { points, pointString };
-}
-
-const defaultMargin = { top: 40, left: 80, right: 80, bottom: 80 };
-
-export default function Example({
-  width,
-  height,
-  levels = 1,
-  data,
-  margin = defaultMargin,
-  selectionFunction
-}) {
-  const y = selectionFunction || (d => d.Expert);
-
-  const xMax = width - margin.left - margin.right;
-  const yMax = height - margin.top - margin.bottom;
-  const radius = Math.min(xMax, yMax) / 2;
-
-  const radialScale = scaleLinear({
-    range: [0, Math.PI * 2],
-    domain: [degrees, 0]
-  });
-
-  const yScale = scaleLinear(
-    {
-      range: [0, radius],
-      domain: [0, Math.max(...data.map(y))]
-    }
-  );
-
-  const webs = genAngles(data.length);
-  const points = genPoints(data.length, radius);
-  const polygonPoints = genPolygonPoints(data, yScale, y);
-  const zeroPoint = new Point({ x: 0, y: 0 });
-
-  return width < 10 ? null : (
-    <svg width={width} height={height}>
-      <rect fill={background} width={width} height={height} rx={14} />
-      <Group top={height / 2 - margin.top} left={width / 2}>
-        {[...new Array(levels)].map(
-          (_, i) => (
-              <>
-                <LineRadial
-                  key={`web-${i}`}
-                  data={webs}
-                  angle={(d) => radialScale(d.angle)}
-                  radius={((i + 1) * radius) / levels}
-                  fill="none"
-                  stroke={silver}
-                  strokeWidth={5}
-                  strokeOpacity={0.8}
-                  strokeLinecap="round"
-                />
-              </>
-            )
-        )}
-        {[...new Array(data.length)].map((_, i) => console.log(i) || (
-          <>
-            <Line
-              key={`radar-line-${i}`}
-              from={zeroPoint}
-              to={points[i]}
-              stroke={silver}
-            />
-          </>
-        ))}
-        <polygon
-          points={polygonPoints.pointString}
-          fill={orange}
-          fillOpacity={0.3}
-          stroke={orange}
-          strokeWidth={1}
-        />
-        {polygonPoints.points.map((point, i) => (
-          <circle
-            key={`radar-point-${i}`}
-            cx={point.x}
-            cy={point.y}
-            r={4}
-            fill={pumpkin}
-          />
-        ))}
-      </Group>
-    </svg>
+  return (
+    <RadarChart
+      data={data}
+      tickFormat={(t) => wideFormat(t)}
+      startingAngle={0}
+      domains={domains}
+      width={width || 400} // The width needs to be fixed
+      height={height || 300}
+      style={{
+        polygons: {
+          strokeWidth: 1,
+          strokeOpacity: 1,
+          fillOpacity: 0.5
+        },
+        labels: {
+          textAnchor: 'middle'
+        },
+        axes: {
+          line: {
+            fillOpacity: 0,
+            strokeWidth: 0,
+            strokeOpacity: 0
+          },
+          ticks: {
+            fillOpacity: 0,
+            strokeOpacity: 0
+          },
+          text: {
+            fillOpacity: 0.3
+          }
+        }
+      }}
+    />
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -2,100 +2,73 @@ import React, { useState } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import data from './data.json';
+import groupBy from 'lodash/groupBy';
+import trim from 'lodash/trim';
 import dynamic from 'next/dynamic';
+
 const Radar = dynamic(() => import('../components/radar/radar'));
 
-// const learningClassification = {
-//   "Want to learn": 0,
-//   "Learning": 1,
-// }
-
-// const learningInitial = {
-//   "Want to learn": 0,
-//   "Learning": 0,
-// }
-
-const expertiseInitial = {
-  "No knowledge": 0,
-  "Basic knowledge": 0,
-  "Competent": 0,
-  "Expert": 0,
-}
-
-// const notInterestedClassification = {
-//   "Not interested": 1
-// }
-
 const nameData = {}
-const expertiseData = [];
+const totalSkills = [];
+
 data.forEach(d => {
   const updatedD = { ...d };
   const regex = /(.+)\[(.+)]/;
-  delete updatedD['Your name'];
-  const nameSkills = []
   Object.keys(updatedD).forEach((currentKey) => {
     const match = currentKey.match(regex);
     const category = match && match[1];
-    const skill = match && match[2];
-    const value = updatedD[currentKey].split(',');
-    nameSkills.push({
-      category,
-      skill,
-      value
+    const skill = match && trim(match[2].toLowerCase());
+    const value = updatedD[currentKey]
+      .split(',')
+      .map((v) => trim(v.toLowerCase()));
+
+    value.forEach(v => {
+      if (v && skill) {
+        totalSkills.push({
+          category,
+          skill,
+          value: v,
+          name: d['Your name']
+        });
+      }
     });
-    const expertiseSkill = expertiseData.find(e => e.skill === skill);
-    if (expertiseSkill) {
-      Object.keys(expertiseInitial).filter(e => value.includes(e)).forEach((level) => {
-        const expertiseSkillIndex = expertiseData.indexOf(expertiseSkill);
-        expertiseData[expertiseSkillIndex] = { ...expertiseSkill, [level]: expertiseSkill[level] + 1 };
-      });
-    } else {
-      expertiseData.push({ skill, ...expertiseInitial});
-    }
   });
-  const total = Object.keys(expertiseData).filter(Boolean)
-  nameData[d['Your name']] = nameSkills;
 })
 
+const groupedSkills = groupBy(totalSkills, 'value')
+const skillData = [];
+Object.keys(groupedSkills).forEach((skill) => {
+  const valueGrouped = groupBy(groupedSkills[skill], 'skill');
+  const valueGroupedNumber = {};
+  Object.keys(valueGrouped).forEach(key => {
+    valueGroupedNumber[key] = valueGrouped[key].length;
+  });
+  skillData.push({
+    name: skill,
+    ...valueGroupedNumber
+  });
+});
+
 export default function Home() {
-  const [selection, setSelection] = useState('Expert');
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Vizzuality frontend</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>Vizzuality Frontend skills</h1>
-        {Object.keys(expertiseInitial).map(e => (
-          <button onClick={() => setSelection(e)}>{e}</button>
-        ))}
         <Radar
           width={600}
           height={600}
-          data={expertiseData}
-          selectionFunction={(d) => d[selection]}
+          data={skillData}
         />
         <div className={styles.description}>
           <ul>
             {Object.keys(nameData).map((name) => (
               <li key={name} className={styles.card}>
                 <h2>{name}</h2>
-                {/* <div>
-                  {nameData[name].map((s) => (
-                    <ul>
-                      <li key={name}>
-                        {s.category}
-                        {' / '}
-                        {s.skill}
-                        {' = '}
-                        {s.value.join(' and ')}
-                      </li>
-                    </ul>
-                  ))}
-                </div> */}
-                {/* <Radar width={300} height={300} /> */}
               </li>
             ))}
           </ul>
