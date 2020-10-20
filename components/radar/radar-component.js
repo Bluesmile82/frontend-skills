0d6acd9e2ee3421abce130e4bd1f5d641260a5a4
Alvaro Leal <pursuance@gmail.com>
2020-10-20 11:10:30 +0200

    Add categories and values filters

--- components/radar/radar-component.js
+++ components/radar/radar-component.js
@@ -1,52 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { format } from 'd3-format';
 import { RadarChart } from 'react-vis';
+import styles from './radar.module.css';
+import cx from 'classnames';
 
 const wideFormat = format('.3r');
 
+const colors = {
+  "competent": 'blue',
+  "learning": 'red',
+  "want to learn": 'magenta',
+  "not interested": 'yellow',
+  "basic knowledge": 'orange',
+  "no knowledge": 'black',
+  "expert": 'green',
+ };
+
 export default function BasicRadarChart(props) {
-  const { width, height, data } = props;
-  // We need the domainNames of all categories, not only basic data
-  const basicData = data.find(d => d.name === 'basic knowledge');
-  const domainNames = Object.keys(basicData).filter(key => key !== 'name');
+  const { width, height, data, skills } = props;
+  console.log(data);
+  const [selectedValues, selectValues] = useState(data.map(d => d.name));
+  const domainNames = skills.map(s => s.skill);
   const domains = domainNames.map(name => ({
     name,
-    domain: [0, 18],
+    domain: [0, 14],
     getValue: (d) => d[name]
-  }))
+  }));
 
+  const dataWithColors = data.map((d) => ({
+    ...d,
+    stroke: colors[d.name],
+    fill: colors[d.name]
+  }));
+  const filterSelectedData = dataWithColors.filter(d => selectedValues.includes(d.name));
+  const handleLegendClick = (value) => {
+    selectedValues.includes(value)
+      ? selectValues(selectedValues.filter((v) => v !== value))
+      : selectValues([...selectedValues, value]);
+  };
   return (
-    <RadarChart
-      data={data}
-      tickFormat={(t) => wideFormat(t)}
-      startingAngle={0}
-      domains={domains}
-      width={width || 400} // The width needs to be fixed
-      height={height || 300}
-      style={{
-        polygons: {
-          strokeWidth: 1,
-          strokeOpacity: 1,
-          fillOpacity: 0.5
-        },
-        labels: {
-          textAnchor: 'middle'
-        },
-        axes: {
-          line: {
-            fillOpacity: 0,
-            strokeWidth: 0,
-            strokeOpacity: 0
+    <>
+      <div className={styles.legend}>
+        <div className={styles.legendTitle}>Legend</div>
+        {dataWithColors.map((d) => (
+          <div className={styles.legendItem}>
+            <span
+              className={styles.legendColor}
+              style={{ backgroundColor: d.fill }}
+            />
+            <button
+              className={cx(styles.legendButton, {[styles.active]: selectedValues.includes(d.name)})}
+              onClick={() => handleLegendClick(d.name)}
+            >
+              {d.name}
+            </button>
+          </div>
+        ))}
+      </div>
+      <RadarChart
+        data={filterSelectedData}
+        tickFormat={(t) => wideFormat(t)}
+        startingAngle={0}
+        domains={domains}
+        width={width || 400}
+        height={height || 300}
+        margin={{ left: 100, right: 100, top: 100, bottom: 100 }}
+        style={{
+          polygons: {
+            strokeWidth: 1,
+            strokeOpacity: 1,
+            fillOpacity: 0.5
           },
-          ticks: {
-            fillOpacity: 0,
-            strokeOpacity: 0
-          },
-          text: {
-            fillOpacity: 0.3
+          axes: {
+            line: {
+              fillOpacity: 0,
+              strokeWidth: 0,
+              strokeOpacity: 0
+            },
+            ticks: {
+              fillOpacity: 0,
+              strokeOpacity: 0
+            },
+            text: {
+              fillOpacity: 0.3
+            }
           }
-        }
-      }}
-    />
+        }}
+      />
+    </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import data from './data.json';
+import data from '../data/data.json';
 import groupBy from 'lodash/groupBy';
 import trim from 'lodash/trim';
 import dynamic from 'next/dynamic';
-
+import { uniqBy } from 'lodash';
+import cx from 'classnames';
 const Radar = dynamic(() => import('../components/radar/radar'));
 
 const nameData = {}
 const totalSkills = [];
-
+const allSkills = [];
 data.forEach(d => {
   const updatedD = { ...d };
   const regex = /(.+)\[(.+)]/;
+  const parseText = (text) => trim(text.toLowerCase());
   Object.keys(updatedD).forEach((currentKey) => {
     const match = currentKey.match(regex);
-    const category = match && match[1];
-    const skill = match && trim(match[2].toLowerCase());
+    const category = match && parseText(match[1]);
+    const skill = match && parseText(match[2]);
     const value = updatedD[currentKey]
       .split(',')
-      .map((v) => trim(v.toLowerCase()));
+      .map(parseText);
+
+    if (skill) {
+      allSkills.push({ skill, category });
+    }
 
     value.forEach(v => {
       if (v && skill) {
@@ -34,7 +40,6 @@ data.forEach(d => {
     });
   });
 })
-
 const groupedSkills = groupBy(totalSkills, 'value')
 const skillData = [];
 Object.keys(groupedSkills).forEach((skill) => {
@@ -49,7 +54,17 @@ Object.keys(groupedSkills).forEach((skill) => {
   });
 });
 
+const uniqueSkills = uniqBy(allSkills, 'skill');
+const categorySkills = groupBy(uniqueSkills, 'category');
+
 export default function Home() {
+  const [selectedCategory, selectCategory] = useState(null);
+  let filteredSkills = uniqueSkills;
+  if (selectedCategory) {
+    filteredSkills = uniqueSkills.filter(
+      (skill) => skill.category === selectedCategory
+    );
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -59,10 +74,22 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>Vizzuality Frontend skills</h1>
+        <div className={styles.categories}>
+          {categorySkills &&
+            Object.keys(categorySkills).map((category) => (
+              <button
+                className={cx({ [styles.blue]: category === selectedCategory })}
+                onClick={() => selectCategory(selectedCategory === category ? null : category)}
+              >
+                {category}
+              </button>
+            ))}
+        </div>
         <Radar
           width={600}
           height={600}
           data={skillData}
+          skills={filteredSkills}
         />
         <div className={styles.description}>
           <ul>
